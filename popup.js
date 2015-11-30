64515c0eb352e860a387082209da1f7a06e9901a
LeeJim <273743552@qq.com>
2015-12-01 00:02:51 +0800

    first version is done

--- popup.js
+++ popup.js
@@ -5,13 +5,37 @@ function click(e) {
       {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
   window.close();
 }
+function f(id) {
+  return document.getElementById(id);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  var button = document.querySelectorAll('#saveButton');
-  	  img = new Image(),
-  	  data = 'url=' + location.href;
+	var	button = f('saveButton');
 
-  img.src = 'www.baidu.com?'+data;
-  document.body.appendChild(img);
+  chrome.tabs.getSelected(null,function(tab){
+    f('webName').value = tab.title;
+  })
+
+  button.onclick = function(){
+  	var img = new Image();
+
+  	img.style.display = 'none';
+  	img.onload = function(){
+  		alert('保存成功');
+  	}
+    chrome.tabs.getSelected(null, function(tab) {
+      var url = tab.url,
+          wName = f('webName').value || '未命名',
+          wTag = f('webTag').value || '',
+          data = [];
+      data.push(url,wName,wTag);
+      var str = data.join('_leejim_');
+      img.src = 'http://www.syxxbst.com/webcollect/save/' + str ;
+      document.body.appendChild(img);
+    });
+  	
+  }
+
+  
 
 });
